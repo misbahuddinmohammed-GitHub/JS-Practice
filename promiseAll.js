4f3ffd1cb0de41a846aa0d445a470f5d488d4b6d
misbahuddinmohammed-GitHub <mdmisbahuddin2001@gmail.com>
2024-08-23 19:20:44 +0530

    Third Commit

--- promiseAll.js
+++ promiseAll.js
@@ -24,3 +24,30 @@ Promise.all([p1, p2, p3])
   .catch((err) => {
     console.error(err);
   });
+
+// PromiseAll Failure Case ------------
+const p4 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("P4 success");
+  }, 3000);
+});
+
+const p5 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject("P5 Fails");
+  }, 1000);
+});
+
+const p6 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("P6 success");
+  }, 2000);
+});
+
+Promise.all([p4, p5, p6])
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.error(err);
+  });

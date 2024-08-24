9c9ad799684ddebadccdda98bcf973858e662e20
misbahuddinmohammed-GitHub <mdmisbahuddin2001@gmail.com>
2024-08-24 11:44:25 +0530

    Seventh Commit

--- Promise.any.js
+++ Promise.any.js
@@ -24,3 +24,33 @@ Promise.any([p1, p2, p3])
     console.error(err);
     console.log(err.errors);
   });
+
+// If all the promises in the array gets rejected then it will give an array of aggregate error.
+// Below is the example of the code
+
+const p4 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject("P4 Success");
+  }, 5000);
+});
+
+const p5 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject("P5 Fails");
+  }, 3000);
+});
+
+const p6 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject("P6 Success");
+  }, 6000);
+});
+
+Promise.any([p4, p5, p6])
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.error(err);
+    console.log(err.errors);
+  });
